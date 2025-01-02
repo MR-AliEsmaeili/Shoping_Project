77468ab54d97142748bcc8d5b294e57317f8d86d
unknown <ali.esmaeili.online@gmail.com>
2025-01-02 14:48:34 +0330

    render products - build component card and create search and category in side bar

---
+++ src/Components/Card.jsx
@@ -0,0 +1,38 @@
+import { BsBasket } from "react-icons/bs";
+import { TbListDetails } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { ShortenText } from "../helpers/helper";
+
+const Card = ({ data }) => {
+  const { id, title, image, price } = data;
+  return (
+    <div className="bg-white  shadow-md rounded-lg overflow-hidden flex flex-col  p-4 transition-transform transform hover:scale-105">
+      <img
+        className="w-full h-40 object-contain mb-4"
+        src={image}
+        alt={title}
+      />
+      <h3 className="text-lg font-semibold  text-gray-800 text-center mb-2">
+        {ShortenText(title)}
+      </h3>
+      <p className="text-gray-600 text-end ml-12 text-sm mb-4">{price} تومان</p>
+      <div className="flex justify-around items-center w-full mt-auto">
+        <Link
+          to={`/products/${id}`}
+          className="text-blue-500 hover:text-blue-700 text-lg"
+          title="جزئیات محصول"
+        >
+          <TbListDetails />
+        </Link>
+        <button
+          className="bg-blue-500 text-white py-1 px-3 rounded-lg hover:bg-blue-600"
+          title="اضافه به سبد خرید"
+        >
+          <BsBasket />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,16 +1,83 @@
+import Card from "../Components/Card";
 import { useProducts } from "../Context/ProductProvider";
+import Loader from "../Components/Loader";
+import { ImSearch } from "react-icons/im";
+import { useState } from "react";
+import { BiCategory } from "react-icons/bi";
 
 const ProductsPage = () => {
+  const [search, setSearch] = useState("");
   const Products = useProducts();
+
+  const categoryHandler = (e) => {
+    const category = e.target.dataset.name; // استفاده از ویژگی data-name
+    console.log(category);
+  };
+
   return (
-    <div className="flex justify-between flex-wrap mt-10">
-      <div>
-        {Products.map((p) => (
-          <p key={p.id}>{p.title}</p>
-        ))}
+    <>
+      <div className="mt-16 flex align-baseline">
+        <input
+          className="p-3 rounded-lg shadow-sm shadow-gray-400 outline-none"
+          type="text"
+          placeholder="جستجو محصول ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button className="p-3 mx-3 shadow-sm shadow-gray-400 rounded-lg bg-blue-500">
+          <ImSearch color="white" />
+        </button>
       </div>
-      <div>side bar</div>
-    </div>
+      <div className="flex flex-col md:flex-row mt-10 gap-6">
+        {Products.length === 0 ? (
+          <Loader />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 flex-1">
+            {Products.map((p) => (
+              <Card key={p.id} data={p}></Card>
+            ))}
+          </div>
+        )}
+        <div className="flex flex-col align-baseline w-full md:w-1/4 h-fit bg-gray-100 p-4 py-10 rounded-lg shadow-md">
+          <div className="flex align-baseline place-items-center mb-2">
+            <BiCategory size={20} />
+            <h2 className="text-lg font-semibold mr-3">دسته بندی ها</h2>
+          </div>
+          <ul className="mt-4" onClick={categoryHandler}>
+            <li
+              className="font-normal pb-3 hover:text-blue-600 cursor-pointer"
+              data-name="All"
+            >
+              همه
+            </li>
+            <li
+              className="font-normal pb-3 hover:text-blue-600 cursor-pointer"
+              data-name="men's clothing"
+            >
+              پوشاک مردانه
+            </li>
+            <li
+              className="font-normal pb-3 hover:text-blue-600 cursor-pointer"
+              data-name="women's clothing"
+            >
+              پوشاک زنانه
+            </li>
+            <li
+              className="font-normal pb-3 hover:text-blue-600 cursor-pointer"
+              data-name="jewelery"
+            >
+              جواهرات
+            </li>
+            <li
+              className="font-normal pb-3 hover:text-blue-600 cursor-pointer"
+              data-name="electronics"
+            >
+              لوازم الکترونیکی
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
